58c079a214c8890f9fca917ba4be60293eefd306
edivaner <edivanerfernandes18@gmail.com>
2021-09-19 21:05:32 -0300

    adicionando migrate

---
+++ database/migrations/2021_09_18_194112_alter_fornecedores_novas_colunas.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterFornecedoresNovasColunas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('fornecedores', function(Blueprint $table){
+            $table->string('uf', 2);
+            $table->string('email', 150);
+            $table->string('site', 150);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        schema::table('fornecedores', function(Blueprint $table){
+            $table->dropColumn('uf');
+            $table->dropColumn('email');
+            $table->dropColumn('site');
+            $table->dropColumn(['uf','email', 'site']);
+        });
+    }
+}
